a9f8cb9e4ec718d02fc8cefbb93ef4bb76f404e1
Aditya Subono <adityasubono@gmail.com>
2020-03-29 15:02:07 +0700

    add postgres

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
 var logger = require('morgan');
-const cors = require("cors");
+// const cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,7 +16,7 @@ var corsOptions = {
   origin: "http://localhost:4201"
 };
 
-app.use(cors());
+// app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
