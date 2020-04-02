5472a91af5cbae04ca6b4e50c42f944ef2d87eba
Khairunnisa <khairunnisa2@btpn.com>
2020-04-02 21:59:09 +0700

    [nisa] update cors

--- app.js
+++ app.js
@@ -16,18 +16,12 @@ var corsOptions = {
   origin: "http://localhost:4201"
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
